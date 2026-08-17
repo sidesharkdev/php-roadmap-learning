13ad30585785ed8cc9f62fae084ecdc81bc8c9b3
sidesharkdev <faqihxavierlf@gmail.com>
2026-08-17 10:05:48 +0700

    feat(syntax): add basic PHP syntax examples

---
+++ 01-fundamentals/05-basic-php-syntax/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Basic PHP Syntax
+
+$name    = "Xavier";
+$message = "Welcome to PHP!";
+
+echo $message . "\n";
+echo "Hello, " . $name . "!\n";
+
+/*
+ * PHP statements end with a semicolon.
+ */
+echo "This is a PHP statement.\n";
