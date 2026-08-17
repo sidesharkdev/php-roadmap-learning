3834f109d29e9c69cf3885aed4791cd6c497ee05
sidesharkdev <faqihxavierlf@gmail.com>
2026-08-17 09:51:16 +0700

    feat(introduction): add PHP runtime example

---
+++ 01-fundamentals/02-what-is-php/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "What is PHP?\n";
+echo "PHP is running successfully.\n";
+
+echo "\nPHP Version: " . PHP_VERSION . "\n";
+echo "Operating System: " . PHP_OS_FAMILY . "\n";
+echo "SAPI: " . PHP_SAPI . "\n";
